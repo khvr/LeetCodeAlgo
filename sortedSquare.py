2710ef2d8171622bf2e0d3bc3220688799b54874
khvr <kalyanaraman.ha@northeastern.edu>
2021-03-17 20:14:25 -0400

    sortedSquare

---
+++ sortedSquare.py
@@ -0,0 +1,30 @@
+from typing import List
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            if nums[i]>0:
+                break
+        right=i
+        left=i-1
+        result=[]
+        while right <= len(nums) and left >= 0:
+            if nums[right]**2<nums[left]**2:
+                result.append(nums[right]**2)
+                right+=1
+            elif nums[left]**2<nums[right]**2:
+                result.append(nums[left]**2)
+                left-=1
+        
+        while right <= len(nums):
+            result.append(nums[right]**2)
+            right+=1
+        
+        while left >=0:
+            result.append(nums[left]**2)
+            left-=1
+        return result
+            
+                
+
+sol = Solution()
+print(sol.sortedSquares([-4,-1-0,3,10])
